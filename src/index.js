e821d6b3aeadf88e4885ae459f057e06c2120dc5
Jonathan Moutier <djo.moutier@gmail.com>
2018-01-20 17:57:37 +0100

    delete unused middleware

--- src/index.js
+++ src/index.js
@@ -13,26 +13,4 @@ const app = new Koa()
 app
   .use(bodyParser())
   .use(cors())
-  .use(async (ctx, next) => {
-    const parseUrl = parser(ctx.request.url)
-    const querys = queryString.parse(parseUrl.query)
-    querys.api_key = process.env.GIPHY_API_KEY
-    querys.limit = 2
-    ctx.request.url = `http://api.giphy.com/v1${parseUrl.pathname}?${queryString.stringify(querys)}`
-
-    await next()
-    
-    delete ctx.body.meta
-    delete ctx.body.pagination
-  })
-  .use(async (ctx, next) => {
-    try {
-      const res = (await axios.get(ctx.request.url)).data
-      ctx.body = res
-    } catch (e) {
-      throw new Error(e)
-    }
-    await next()
-  })
-
 app.listen(3000)
